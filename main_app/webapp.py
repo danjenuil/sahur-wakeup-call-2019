c6ea602a8f65df1e679b262789b892f54de51241
danjenuil <dannyroy1002@gmail.com>
2019-04-24 17:34:08 +0800

    Modify files for production

--- main_app/webapp.py
+++ main_app/webapp.py
@@ -1,24 +1,7 @@
 import plivo
 from flask import url_for
-from flask_apscheduler import APScheduler
 
 from . import app, views
-from .models import Subscriber
 
-# scheduler = APScheduler()
-
-# @scheduler.task('cron', id='send_sahur_reminder')
-# def send_sahur_reminder_call():
-#     subscribers = Subscriber.query.all()
-#     delimiter = "<"
-#     numbers = []
-#     client = plivo.phlo.RestClient(auth_id=app.config['PLIVO_AUTH_ID'], auth_token=app.config['PLIVO_AUTH_TOKEN'])
-#     phlo = client.phlo.get("78d152c4-6635-4f9c-a9a9-d3bbe5bdad19")
-#     if subscribers:
-#         people_list = Subscriber.query.filter_by(confirmed=1).all()
-#         for person in people_list:
-#             numbers.append(person) 
-#         to = delimiter.join(numbers)
-#         data = {"from": "0149523860", "to": to}
-#         response = phlo.run(data=data)
-#         print("Sahur wakeup call sent. Status: {}".format(response))
+if __name__ == "__main__":
+    app.run()
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from main_app.webapp import app
+
+if __name__=="__main__":
+    app.run()
